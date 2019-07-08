be8271f3cbe958849e394807197d6179bbff5751
KMONISH <monishkb1999@gmail.com>
2019-07-07 22:37:52 -0500

    python

---
+++ python/inputlist.py
@@ -0,0 +1,6 @@
+lit = []
+n = int(input("enter the number of element:"))
+for i in range(0,n):
+   ele = int(input())
+   lit.append( ele )
+   print(lit)
---
+++ python/overloading.py
@@ -0,0 +1,7 @@
+def getdata(a , b):
+    f=a*b
+    print(f)
+def getdata(a, b ,c):
+    f = a*b*c
+    print(f)
+getdata(2,3,4)
